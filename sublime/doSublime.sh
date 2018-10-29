103f106d4db6336ff927b08b9c5672fd70ac3558
michondr <michondr@gmail.com>
2018-10-29 23:23:08 +0100

    flameshot fixes

--- sublime/doSublime.sh
+++ sublime/doSublime.sh
@@ -1,7 +1,7 @@
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -;
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list;
-sudo apt --assume-yes update
-sudo apt --assume-yes install sublime-text libgtk2.0-0
+sudo apt --assume-yes -qq update
+sudo apt --assume-yes -qq install sublime-text libgtk2.0-0
 
 # set my custom keboard shortcuts
 cp roles/michondr.sublime-keymap ~/.config/sublime-text-3/Packages/User/Default.sublime-keymap
