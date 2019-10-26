4db3b8efbbeec5fdbdf47e74ae1a2d7cc6064e27
michondr <michondr@gmail.com>
2019-10-26 20:32:58 +0200

    mysql installation added

---
+++ tasks/programs/mysql/doMysql.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo apt --assume-yes install mysql-server
+
+#this component uninstalling probably works. tried to uninstall as much as I could. do it manually if you'll be unable to set root password
+sudo mysql -u root -Bse "UNINSTALL COMPONENT 'file://component_validate_password';"
+sudo mysql -u root -Bse "ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';"
+sudo mysql -u root --password=root -Bse "FLUSH PRIVILEGES;"
--- tasks/programs/packages/doPackages.sh
+++ tasks/programs/packages/doPackages.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-sudo apt --assume-yes install vim git htop chromium-browser curl python-pip ansible exfat-fuse exfat-utils xclip flameshot awscli
+sudo apt --assume-yes install vim git htop chromium-browser curl python-pip python-pytest pipenv ansible exfat-fuse exfat-utils xclip flameshot awscli
 
 # composer
 sudo curl -sS https://getcomposer.org/installer -o ~/composer-setup.php
