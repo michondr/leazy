fbaf6d1c20341f83477a9a95fe4e98b7da2b615d
michondr <michondr@gmail.com>
2019-10-27 00:00:41 +0200

    fixing jetbrains exports, git aliases downlaod

--- tasks/programs/git/doGit.sh
+++ tasks/programs/git/doGit.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-gitconfig_folder='~/.config/gitconfig'
+gitconfig_folder='/home/michondr/.config/gitconfig'
 
-rm -rf ${gitconfig_folder}
-git clone https://github.com/michondr/gitconfig.git ${gitconfig_folder}
+rm -rf $gitconfig_folder
+echo $gitconfig_folder
+git clone https://github.com/michondr/gitconfig.git $gitconfig_folder
 
 cp tasks/programs/git/templates/home.gitconfig ~/.gitconfig
