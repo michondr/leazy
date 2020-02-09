95446f00512fda9cb57c2c74810439c9093dac92
michondr <michondr@gmail.com>
2020-02-10 00:30:55 +0100

    adding speedy shortcuts

--- tasks/programs/jetbrains/doJetbrains.sh
+++ tasks/programs/jetbrains/doJetbrains.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+##
+# moving jetbrain config location under ~/.config/
+##
+
 # init
+user_dir=/home/$USER/_$USER
 conf_dir=/home/$USER/.config/jetbrains
 
 mkdir -p "$conf_dir"
@@ -20,6 +25,10 @@ sudo sed -i -e "s@CONF_DIR@$conf_dir/PhpStorm@g" $conf_dir/PhpStorm/idea.propert
 cp tasks/programs/jetbrains/templates/idea.properties $conf_dir/PyCharm/idea.properties
 sudo sed -i -e "s@CONF_DIR@$conf_dir/PyCharm@g" $conf_dir/PyCharm/idea.properties
 
+##
+# installing jetbrains IDEs
+##
+
 # idea
 sudo snap install intellij-idea-ultimate --classic
 
@@ -28,3 +37,22 @@ sudo snap install phpstorm --classic
 
 # pycharm
 sudo snap install pycharm-professional --classic
+
+
+##
+# adding speedy shortcuts automatically
+##
+
+# clone to user dir
+git clone git@github.com:michondr/speedy-shortcuts.git $user_dir
+
+# symlink to jetbrains dir
+ln -s $user_dir/speedy-shortcuts $conf_dir
+
+# symlink from jetbrains dir to Phpstorm's config
+## TODO: finish symlinks
+#ln -s -f $conf_dir/speedy-shortcuts/options/ide.general.xml $conf_dir/PhpStorm/config/options/
+#ln -s -f $conf_dir/speedy-shortcuts/options/keymap.xml $conf_dir/PhpStorm/config/options/
+#ln -s -f $conf_dir/speedy-shortcuts/options/macros.xml $conf_dir/PhpStorm/config/options/
+#ln -s -f $conf_dir/speedy-shortcuts/keymaps/michondrKeymap.xml $conf_dir/PhpStorm/config/keymaps/
+#ln -s -f $conf_dir/speedy-shortcuts/tools/External Tools.xml $conf_dir/PhpStorm/config/tools/
