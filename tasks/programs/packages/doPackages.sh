060927d7dbe50ded1891f986198af4f3751b09e8
michondr <michondr@gmail.com>
2020-10-28 17:17:11 +0100

    fixed issues after install

--- tasks/programs/packages/doPackages.sh
+++ tasks/programs/packages/doPackages.sh
@@ -9,8 +9,13 @@ sudo apt --assume-yes install default-jdk default-jre
 sudo apt --assume-yes install python3-pip python3-pytest pipenv
 
 # php crap
-sudo apt --assume-yes install php7.4-fpm php7.4-cli php7.4-common php7.4-curl php7.4-json php7.4-mysql php7.4-xml php7.4-mbstring php7.4-gd php7.4-df php7.4-pdo-mysql php7.4-dev phpunit
-
+sudo apt --assume-yes install php7.4-fpm php7.4-cli php7.4-common php7.4-curl php7.4-json php7.4-mysql php7.4-xml php7.4-mbstring php7.4-gd php7.4-pdo-mysql php7.4-dev php7.4-imap php7.4-soap phpunit
+wget http://www.bytereef.org/software/mpdecimal/releases/mpdecimal-2.5.0.tar.gz
+cd mpdecimal-2.5.0.tar.gz
+./configure
+make
+sudo make install
+pecl install decimal
 
 # composer
 sudo curl -sS https://getcomposer.org/installer -o ~/composer-setup.php
@@ -26,3 +31,8 @@ sudo unlink /var/run/supervisor.sock
 
 # aws fuzzy
 pip install aws-fuzzy-finder
+
+# symfony bin
+wget https://get.symfony.com/cli/installer -O - | bash
+sudo mv /home/michondr/.symfony/bin/symfony /usr/local/bin/symfony
+rm ~/.symfony/ -rf
---
+++ tasks/system-config/fish/templates/config.fish
@@ -0,0 +1,6 @@
+set fish_greeting
+
+set -x PHPSTORM_PROPERTIES ~/.config/jetbrains/PhpStorm/idea.properties
+set -x IDEA_PROPERTIES ~/.config/jetbrains/IntelliJIdea/idea.properties
+set -x PYCHARM_PROPERTIES ~/.config/jetbrains/PyCharm/idea.properties
+
