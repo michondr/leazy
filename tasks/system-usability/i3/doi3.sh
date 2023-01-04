7705e3784db2095b460c1492e0bf3f5711da750e
Ondřej Michálek <michondr@gmail.com>
2023-01-04 20:19:31 +0100

    add playerctl to installed apps, remove left-hand-mouse shortcuts for flameshot

--- tasks/system-usability/i3/doi3.sh
+++ tasks/system-usability/i3/doi3.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # all of i3 crap that is needed
 
-sudo apt --assume-yes install i3 i3blocks arandr autorandr rofi compton fonts-font-awesome feh flameshot
+sudo apt --assume-yes install i3 i3blocks arandr autorandr rofi compton fonts-font-awesome feh flameshot playerctl
 
 cp tasks/system-usability/i3/config/* ~/.config/i3
 
