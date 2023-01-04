6b5f1147b98d8d3cc190b3d8bc9e56cd47c35e95
Ondřej Michálek <michondr@gmail.com>
2023-01-04 21:09:50 +0100

    rename base i3 dir

--- i3/init.sh
+++ i3/init.sh
@@ -3,7 +3,7 @@
 
 sudo apt --assume-yes install i3 i3blocks arandr autorandr rofi compton fonts-font-awesome feh flameshot playerctl
 
-ln -s ~/_michondr/leazy/i3/config/ ~/.config/i3
+ln -s ~/_michondr/leazy/i3/setup/ ~/.config/i3
 chmod +x config/scripts/date.sh
 chmod +x config/scripts/start_pavucontroll_clear_spotify.sh
 
