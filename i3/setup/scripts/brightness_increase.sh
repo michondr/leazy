c21e36cf23806fc0247dc307b7642f26b236d58a
Ondřej Michálek <michondr@gmail.com>
2023-07-01 22:07:15 +0200

    brightness control - move to light

--- i3/setup/scripts/brightness_increase.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#don't forget to run: sudo usermod -aG video michondr
-
-LOCATION=/sys/class/backlight/amdgpu_bl0/brightness
-VALUE=$(cat $LOCATION)
-
-echo $(($VALUE+25)) > $LOCATION
--- i3/setup/scripts/i3blocks/brightness_info.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#don't forget to run: sudo usermod -aG video michondr
-
-VALUE=$(cat /sys/class/backlight/amdgpu_bl0/brightness)
-MAX_VALUE=$(cat /sys/class/backlight/amdgpu_bl0/max_brightness)
-
-echo $VALUE of $MAX_VALUE
