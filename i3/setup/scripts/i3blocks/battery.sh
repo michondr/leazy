4f133593389ddf380cb08ff1e444243e151f61ef
Ondřej Michálek <michondr@gmail.com>
2023-07-01 22:25:05 +0200

    fix i3blocks format

--- i3/setup/scripts/i3blocks/battery.sh
+++ i3/setup/scripts/i3blocks/battery.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 BAT=$(acpi -b | grep 'Battery 1' | grep -E -o '[0-9]+?%')
+BAT_TEXT=$(acpi -b | grep 'Battery 1' | grep -E -o '([0-9]+?%.*?$)')
 
 # Full and short texts
-echo $(acpi -b | grep 'Battery 1' | cut -d: -f2-)
-echo "BAT: $BAT"
+echo " $BAT_TEXT"
+echo " $BAT_TEXT"
 
 # Set urgent flag below 5% or use orange below 20%
 [ ${BAT%?} -ge 95 ] && echo "#00FF00"
 [ ${BAT%?} -le 20 ] && echo "#FF8000"
 
 # Send notification when battery is under 10%
-[ ${BAT%?} -le 10 ] && notify-send "Battery alert" "battery is under 10%"
+[ ${BAT%?} -le 10 ] && notify-send "Battery alert" "battery is at ${BAT}"
 
 exit 0
