d68f59481fa56a7fdcbcc87e1d074eb65301b262
Ondřej Michálek <michondr@gmail.com>
2023-09-08 12:00:21 +0200

    update cs to add safe directory

--- fish/functions/cs.fish
+++ fish/functions/cs.fish
@@ -1,3 +1,4 @@
 function cs
+  docker exec -it (docker ps --filter NAME=autodevelo-php --format '{{.ID}}') sh -c 'git config --global --add safe.directory /var/www' $argv;
   docker exec -it (docker ps --filter NAME=autodevelo-php --format '{{.ID}}') sh -c 'composer phpcbf:changed ; composer phpcs:changed ; composer phpstan' $argv;
 end
